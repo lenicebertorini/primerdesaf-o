7d4d23d4787f11ecb4a0b63d8d8fce6be9404c5f
Lenice Bertorini <93561975+lenicebertorini@users.noreply.github.com>
2021-11-03 15:10:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+/* ¡BIENVENIDOS A LENNY´S BURGERS!"*/ 
+    
+    var nombre = prompt("Ingresá tu nombre")
+    alert (" Bienvenido/a "  +   nombre) 
+    alert("¡Vas a probar las hamburguesas más ricas de la ciudad!");
+
+    var edad= prompt ("Ingresá tu edad")
+    
+alert ("¿Querés hacer tu pedido?")
+
+alert ("SORPRESA!!!")
+alert ("Ganaste" + edad + "% de descuento en tu combo del día!!!")
+
+var email= prompt ("Ingresá tu e-mail")
+var horario= ("¿Querés recibir más descuentos en tu e-mail? Respondé SI o NO") 
+
+alert ("Excelente elección.Te enviaremos más promociones a tu correo electrónico:" + email )
